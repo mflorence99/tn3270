6c96f81e1c6d4036185d8edda16ab467af4b1731
Mark Florence <mflo999@gmail.com>
2018-04-13 10:06:53 -0400

    Attempt to publish properly

--- publish.sh
+++ publish.sh
@@ -1,11 +1,9 @@
 npm run build
 git add publish.sh
-git add lib/*
 git add src/*
 git commit -m "Prepare to publish"
 git push origin master
 set -e
-npm run build
 npm version patch
 npm publish
 pushd ../el-3270
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export * from './tn3270';
